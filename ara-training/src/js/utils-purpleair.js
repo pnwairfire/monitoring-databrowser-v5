9cd5ec5f242f8f0a9fe5708e4d47808b3ca5f074
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2025-06-13 12:57:23 -0700

    improved ara-training/Makefile

--- ara-training/src/js/utils-purpleair.js
+++ ara-training/src/js/utils-purpleair.js
@@ -8,7 +8,7 @@ import Papa from "papaparse";
 // Get PurpleAir CSV data
 export async function getPurpleAirData(id) {
   const url =
-    "https://airfire-data-exports.s3.us-west-2.amazonaws.com/ara-training-2025/sequence-a/purple_air/timeseries/weekly/" +
+    "https://airfire-data-exports.s3.us-west-2.amazonaws.com/ara-training-2025/sequence-c/purple_air/timeseries/weekly/" +
     id +
     ".csv";
   const response = await fetch(url);
